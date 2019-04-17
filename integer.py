84d44d806ebc84ff0e019b97055008f787f052f0
Yada <ydt.hran2@gmail.com>
2019-04-17 20:23:25 +0900

    Add get_factors

--- integer.py
+++ integer.py
@@ -14,3 +14,20 @@ def get_divisors(n):
             if n // i != i:
                 ret.append(n // i)
     return ret
+
+
+def get_factors(n):
+    """
+    素因数分解
+    :param int n:
+    :type: list of int
+    """
+    ret = []
+    i = 2
+    while 2 <= i <= n:
+        if n % i == 0:
+            ret.append(i)
+            n //= i
+        else:
+            i += 1
+    return ret
