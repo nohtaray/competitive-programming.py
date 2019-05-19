d322bfde344334a2d777dd31c44973dcd6d45285
Yada <ydt.hran2@gmail.com>
2019-05-19 17:18:44 +0900

    Use itertools.accumulate

--- list.py
+++ list.py
@@ -1,31 +1,39 @@
-def cummax(it, first=-float('inf')):
+import itertools
+import operator
+
+
+def cummax(it):
     """
     np.maximum.accumulate(arr) でもよさそう
     https://stackoverflow.com/questions/33569668/numpy-max-vs-amax-vs-maximum
 
     累積 max
     :param collections.Iterable it:
-    :param float first:
     :return:
     """
-    cm = first
-    ret = []
-    for v in it:
-        cm = max(v, cm)
-        ret.append(cm)
-    return ret
+    return list(itertools.accumulate(it, max))
 
 
-def cummin(it, first=float('inf')):
+def cummin(it):
     """
     累積 min
     :param collections.Iterable it:
-    :param float first:
     :return:
     """
-    cm = first
-    ret = []
-    for v in it:
-        cm = min(v, cm)
-        ret.append(cm)
-    return ret
+    return list(itertools.accumulate(it, min))
+
+
+def cumsum(it):
+    """
+    累積和
+    :param collections.Iterable it:
+    :return:
+    """
+    return list(itertools.accumulate(it, operator.add))
+
+
+if __name__ == '__main__':
+    li = [3, 1, 4, 1, 5, 9, 2, 6]
+    assert cummax(li) == [3, 3, 4, 4, 5, 9, 9, 9]
+    assert cummin(li) == [3, 1, 1, 1, 1, 1, 1, 1]
+    assert cumsum(li) == [3, 4, 8, 9, 14, 23, 25, 31]
