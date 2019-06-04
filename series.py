8452d21bbb0bdf90f730f0577ebbbb5652fc7a2a
Yada <ydt.hran2@gmail.com>
2019-06-04 19:35:24 +0900

    Update series.py

--- series.py
+++ series.py
@@ -22,3 +22,56 @@ def triangular_pyramid(n):
     """
     assert n >= 1
     return n * (n + 1) * (n + 2) // 6
+
+
+def factorials(max, mod=None):
+    """
+    階乗 0!, 1!, 2!, ..., max!
+    :param int max:
+    :param int mod:
+    :return:
+    """
+    ret = [1]
+    n = 1
+    if mod:
+        for i in range(1, max + 1):
+            n *= i
+            n %= mod
+            ret.append(n)
+    else:
+        for i in range(1, max + 1):
+            n *= i
+            ret.append(n)
+    return ret
+
+
+# https://atcoder.jp/contests/abc066/submissions/5721975
+def mod_invs(max, mod):
+    """
+    逆元のリスト 0 から max まで
+    https://atcoder.jp/contests/abc127/submissions/5630531
+    ここから。あんまり良くわかってない
+    :param max:
+    :param mod:
+    :return:
+    """
+    invs = [1] * (max + 1)
+    for x in range(2, max + 1):
+        invs[x] = (-(mod // x) * invs[mod % x]) % mod
+    return invs
+
+
+def get_ncrs(n, mod):
+    """
+    nC_0, nC_1, nC_2, ..., nC_n までのリスト
+    :param n:
+    :param mod:
+    :return:
+    """
+    invs = mod_invs(n, mod)
+    ret = [1]
+    ncr = 1
+    for i in range(1, n + 1):
+        ncr = ((ncr * (n - i + 1) % mod) * invs[i]) % mod
+        ret.append(ncr)
+    return ret
