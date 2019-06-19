71db7ae251a557affb61e344380bcc26086d1cb7
Yada <ydt.hran2@gmail.com>
2019-06-20 01:41:02 +0900

    Bind mod parameter

--- mod_int.py
+++ mod_int.py
@@ -1,23 +1,31 @@
-class ModInt:
-    def __init__(self, value, mod):
-        self.value = value % mod
-        self._mod = mod
+def ModInt(mod):
+    class _ModInt:
+        def __init__(self, value):
+            self.value = value % mod
 
-    def __add__(self, other):
-        if isinstance(other, ModInt):
-            return ModInt(self.value + other.value, self._mod)
-        else:
-            return ModInt(self.value + other, self._mod)
+        def __add__(self, other):
+            if isinstance(other, _ModInt):
+                return _ModInt(self.value + other.value)
+            else:
+                return _ModInt(self.value + other)
 
-    def __mul__(self, other):
-        if isinstance(other, ModInt):
-            return ModInt(self.value * other.value, self._mod)
-        else:
-            return ModInt(self.value * other, self._mod)
+        def __mul__(self, other):
+            if isinstance(other, _ModInt):
+                return _ModInt(self.value * other.value)
+            else:
+                return _ModInt(self.value * other)
 
-    def __truediv__(self, other):
-        # TODO: 実装
-        raise NotImplementedError()
+        def __truediv__(self, other):
+            # TODO: 実装
+            raise NotImplementedError()
 
-    def __repr__(self):
-        return str(self.value)
+        def __repr__(self):
+            return str(self.value)
+
+    return _ModInt
+
+
+if __name__ == '__main__':
+    MI7 = ModInt(mod=7)
+    assert str(MI7(1) + MI7(8)) == '2'
+    assert str(MI7(1) + 8) == '2'
