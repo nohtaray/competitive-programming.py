f86a13c019ffb87d059ef9b4cfc4735368263255
Yada <ydt.hran2@gmail.com>
2019-08-11 14:33:43 +0900

    BellmanFord パスに含まれない負の閉路を無視する

--- graph.py
+++ graph.py
@@ -26,8 +26,56 @@ def floyd_warshall(*args, **kwargs):
     return scipy.sparse.csgraph.floyd_warshall(*args, **kwargs)
 
 
-def bellman_ford(*args, **kwargs):
-    return scipy.sparse.csgraph.bellman_ford(*args, **kwargs)
+def bellman_ford(graph, from_v, to_v):
+    """
+    到達できないなら INF、負閉路があったら -INF
+    :param list of (list of (int, int)) graph: (to, cap) の隣接リスト
+    :param int from_v:
+    :param int to_v:
+    :rtype: int
+    """
+
+    def reachable(graph, from_v):
+        """
+        graph 上で from_v から到達できるかどうかのリスト
+        :param graph:
+        :param from_v:
+        """
+        ret = [False] * len(graph)
+        ret[from_v] = True
+        stack = [from_v]
+        while stack:
+            v = stack.pop()
+            for u, _ in graph[v]:
+                if not ret[u]:
+                    ret[u] = True
+                    stack.append(u)
+        return ret
+
+    # from_v から到達でき、かつ to_v へ到達できる頂点のみ考える
+    # これら以外には負閉路があっても関係ない
+    vertices = []
+    revs = [[] for _ in range(len(graph))]
+    for v, ud in enumerate(graph):
+        for u, d in ud:
+            revs[u].append((v, d))
+    for v, (r1, r2) in enumerate(zip(reachable(graph, from_v), reachable(revs, to_v))):
+        if r1 & r2:
+            vertices.append(v)
+
+    dist = [float('inf')] * len(graph)
+    dist[from_v] = 0
+    updated = True
+    for _ in range(len(graph) + 1):
+        updated = False
+        for v in vertices:
+            for u, d in graph[v]:
+                if dist[v] + d < dist[u]:
+                    dist[u] = dist[v] + d
+                    updated = True
+        if not updated:
+            break
+    return -float('inf') if updated else dist[to_v]
 
 
 def johnson(*args, **kwargs):
