2bd15a587d92e7d5b8a94416584c2f6642b5b2e2
Yada <ydt.hran2@gmail.com>
2019-12-24 00:22:59 +0900

    図形を描画できるようにする

--- debug.py
+++ debug.py
@@ -1,6 +1,11 @@
 import functools
 import os
 
+import matplotlib.patches as pat
+import matplotlib.pyplot as plt
+
+import libs.geometry as geo
+
 
 def debug(fn):
     if not os.getenv("LOCAL"):
@@ -23,3 +28,25 @@ def debug(fn):
         return ret
 
     return wrapper
+
+
+def plot_figure(*figures):
+    alpha = 0.4
+    ax = plt.axes()
+    for f in figures:
+        # isinstance だとコピペしたクラスが別のクラスとして扱われてしまうのでクラス名で文字列比較
+        if type(f).__name__ == geo.Polygon.__name__:
+            ax.add_patch(pat.Polygon([(p.x, p.y) for p in f.points], alpha=alpha))
+        elif type(f).__name__ == geo.Circle.__name__:
+            ax.add_patch(pat.Circle((f.o.x, f.o.y), f.r, alpha=alpha))
+        elif type(f).__name__ == geo.Segment.__name__:
+            ax.plot((f.p1.x, f.p2.x), (f.p1.y, f.p2.y))
+        elif type(f).__name__ == geo.Point.__name__:
+            ax.plot((f.x,), (f.y,), 'o')
+        elif type(f).__name__ == geo.Line.__name__:
+            raise NotImplementedError()
+        else:
+            raise NotImplementedError()
+    plt.axis('scaled')
+    ax.set_aspect('equal')
+    plt.show()
