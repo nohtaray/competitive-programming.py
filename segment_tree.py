b1957cdb56b84cd861069d0fbd8df37f1ce5121c
Yada <ydt.hran2@gmail.com>
2019-12-27 01:17:04 +0900

    Update SegmentTree
    再帰しないようにした

--- segment_tree.py
+++ segment_tree.py
@@ -1,88 +1,66 @@
 import operator
+from functools import reduce
 
 
 class SegmentTree:
-    # http://tsutaj.hatenablog.com/entry/2017/03/29/204841
-    def __init__(self, size, fn=operator.add, default=None, initial_values=None):
+    # http://codeforces.com/blog/entry/18051
+    def __init__(self, values, op=operator.add):
         """
-        :param int size:
-        :param callable fn: 区間に適用する関数。引数を 2 つ取る。min, max, operator.xor など
-        :param default:
-        :param list initial_values:
+        :param list values:
+        :param callable op: 結合律を満たす二項演算
         """
-        default = default or 0
-
-        # size 以上である最小の 2 冪を size とする
-        n = 1
-        while n < size:
-            n *= 2
-        self._size = n
-        self._fn = fn
-
-        self._tree = [default] * (self._size * 2 - 1)
-        if initial_values:
-            i = self._size - 1
-            for v in initial_values:
-                self._tree[i] = v
-                i += 1
-            i = self._size - 2
-            while i >= 0:
-                self._tree[i] = self._fn(self._tree[i * 2 + 1], self._tree[i * 2 + 2])
-                i -= 1
+        self._size = len(values)
+        self._op = op
+        tree = [None] * self._size * 2
+        tree[self._size:] = values[:]
+        for i in reversed(range(1, self._size)):
+            tree[i] = self._op(tree[i << 1], tree[i << 1 | 1])
+        self._tree = tree
 
     def set(self, i, value):
         """
-        i 番目に value を設定
+        values[i] = value
         :param int i:
         :param value:
-        :return:
         """
-        x = self._size - 1 + i
-        self._tree[x] = value
-
-        while x > 0:
-            x = (x - 1) // 2
-            self._tree[x] = self._fn(self._tree[x * 2 + 1], self._tree[x * 2 + 2])
+        i += self._size
+        self._tree[i] = value
+        i >>= 1
+        while i > 0:
+            self._tree[i] = self._op(self._tree[i << 1], self._tree[i << 1 | 1])
+            i >>= 1
 
     def add(self, i, value):
         """
-        もとの i 番目と value に fn を適用したものを i 番目に設定
+        values[i] = values[i]・value
         :param int i:
         :param value:
-        :return:
         """
-        x = self._size - 1 + i
-        self.set(i, self._fn(self._tree[x], value))
+        new_value = self._op(self._tree[self._size + i], value)
+        self.set(i, new_value)
 
-    def get(self, from_i, to_i=None, k=0, L=None, r=None):
+    def get(self, l, r=None):
         """
-        [from_i, to_i) に fn を適用した結果を返す
-        :param int from_i:
-        :param int to_i:
-        :param int k: self._tree[k] が、[L, r) に fn を適用した結果を持つ
-        :param int L:
-        :param int r:
-        :return:
+        [l, r) に op を順番に適用した値
+        :param int l:
+        :param int|None r:
         """
-        if to_i is None:
-            return self._tree[self._size - 1 + from_i]
-
-        L = 0 if L is None else L
-        r = self._size if r is None else r
-
-        if from_i <= L and r <= to_i:
-            return self._tree[k]
-
-        if to_i <= L or r <= from_i:
-            return None
-
-        ret_L = self.get(from_i, to_i, k * 2 + 1, L, (L + r) // 2)
-        ret_r = self.get(from_i, to_i, k * 2 + 2, (L + r) // 2, r)
-        if ret_L is None:
-            return ret_r
-        if ret_r is None:
-            return ret_L
-        return self._fn(ret_L, ret_r)
+        if r is None:
+            return self._tree[self._size + l]
+        ret_l = []
+        ret_r = []
+        l += self._size
+        r += self._size
+        while l < r:
+            if l & 1:
+                ret_l.append(self._tree[l])
+                l += 1
+            if r & 1:
+                r -= 1
+                ret_r.append(self._tree[r])
+            l >>= 1
+            r >>= 1
+        return reduce(self._op, ret_l + ret_r)
 
     def __len__(self):
         return self._size
@@ -195,7 +173,7 @@ class LazySegmentTree:
 
 
 if __name__ == "__main__":
-    st = SegmentTree(size=4, fn=operator.add, default=0)
+    st = SegmentTree([0] * 4, op=operator.add)
     st.add(1, 1)
     st.add(2, 2)
     st.add(3, 3)
@@ -203,7 +181,7 @@ if __name__ == "__main__":
     assert st.get(1, 4) == 6
     assert st.get(2, 4) == 5
 
-    st = SegmentTree(size=3, fn=min, default=2 ** 31 - 1)
+    st = SegmentTree([2 ** 31 - 1] * 3, op=min)
     st.set(1, 5)
     assert st.get(0, 1) == 2 ** 31 - 1
     assert st.get(0, 2) == 5
