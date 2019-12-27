f707fe78fa1af9f42f210d1faa4655a3ace598be
Yada <ydt.hran2@gmail.com>
2019-12-28 03:50:33 +0900

    Update BinaryIndexedTree

--- binary_indexed_tree.py
+++ binary_indexed_tree.py
@@ -12,7 +12,6 @@ class BinaryIndexedTree:
         i 番目に w を加える
         :param int i:
         :param int w:
-        :return:
         """
         x = i + 1
         while x <= self._size:
@@ -21,15 +20,13 @@ class BinaryIndexedTree:
 
     def sum(self, i):
         """
-        0 番目から i 番目までの合計
+        [0, i) の合計
         :param int i:
-        :return:
         """
         ret = 0
-        x = i + 1
-        while x > 0:
-            ret += self._bit[x - 1]
-            x -= x & -x
+        while i > 0:
+            ret += self._bit[i - 1]
+            i -= i & -i
         return ret
 
     def __len__(self):
@@ -45,34 +42,3 @@ def compress(li, origin=0):
     """
     *ret, = map({v: i + origin for i, v in enumerate(sorted(set(li)))}.__getitem__, li)
     return ret
-
-
-def count_inversions(li, max=None):
-    """
-    遅いので制限時間シビアなときは PyPy 推奨
-    リストから転倒数 (li[i] > li[j] (i < j) となる (i, j) の組み合わせ数) を返す
-    バブルソートするときに反転する必要がある数。
-    :param numpy.ndarray | list of int li:
-            すべての要素が 0 以上の int である配列。
-            BIT を使うので、マイナスを含んだり最大値が大きい場合は↑の compress か
-            np.argsort か scipy.stats.rankdata を使う。全部 O(logN) だとおもう
-            リストに重複を含む場合は np.argsort は unstable なのでつかわない。
-            https://docs.scipy.org/doc/numpy/reference/generated/numpy.sort.html
-    :param int max: li の最大値。わかる場合は指定
-    :rtype: int
-    """
-    if not max:
-        max = __builtins__.max(li)
-    bit = BinaryIndexedTree(size=max + 1)
-    ret = 0
-    for i in range(len(li)):
-        ret += i - bit.sum(li[i])
-        bit.add(li[i], 1)
-    return ret
-
-
-if __name__ == "__main__":
-    # https://atcoder.jp/contests/chokudai_s001/tasks/chokudai_S001_j
-    _ = int(input())
-    li = list(map(int, input().split()))
-    print(count_inversions(li, max=100000))
