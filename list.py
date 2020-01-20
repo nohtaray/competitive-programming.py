47c773de71d2c8560e8134ceec070d3e58e938ab
Yada <ydt.hran2@gmail.com>
2020-01-20 15:43:38 +0900

    Add lcm

--- list.py
+++ list.py
@@ -1,12 +1,13 @@
+from collections import Counter, defaultdict
+
+from libs.integer import get_factors
+
+
 def cummax(it, first=-float('inf')):
     """
-    np.maximum.accumulate(arr) でもよさそう
-    https://stackoverflow.com/questions/33569668/numpy-max-vs-amax-vs-maximum
-
     累積 max
     :param collections.Iterable it:
     :param float first:
-    :return:
     """
     cm = first
     ret = []
@@ -21,7 +22,6 @@ def cummin(it, first=float('inf')):
     累積 min
     :param collections.Iterable it:
     :param float first:
-    :return:
     """
     cm = first
     ret = []
@@ -35,13 +35,33 @@ def cumsum(it):
     """
     累積和
     :param collections.Iterable it:
-    :return:
     """
     cs = 0
     ret = []
     for v in it:
         cs += v
         ret.append(cs)
+    return ret
+
+
+def lcm(it, mod):
+    """
+    :param collections.Iterable it:
+    :param int|None mod:
+    :rtype: int
+    """
+    factors = defaultdict(int)
+    for a in it:
+        for f, cnt in Counter(get_factors(a)):
+            factors[f] = max(factors[f], cnt)
+    ret = 1
+    if mod:
+        for f, cnt in factors.items():
+            ret *= pow(f, cnt, mod)
+            ret %= mod
+    else:
+        for f, cnt in factors.items():
+            ret *= pow(f, cnt)
     return ret
 
 
